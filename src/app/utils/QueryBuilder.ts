faba2c26ce638d6e449adf1bfb5dcd3ca06843a7
rakibul-islam2004 <mdrakibulislamandrokib@gmail.com>
2026-03-01 19:30:41 +0600

    feat(utils): implement static and dynamic relationship inclusion in QueryBuilder

--- src/app/utils/QueryBuilder.ts
+++ src/app/utils/QueryBuilder.ts
@@ -307,6 +307,46 @@ export class QueryBuilder<
     return this;
   }
 
+  include(relation: TInclude): this {
+    if (this.selectFields) {
+      return this;
+    }
+
+    this.query.include = { ...this.query.include, ...relation };
+    return this;
+  }
+
+  dynamicInclude(
+    includeConfig: Record<string, unknown>,
+    defaultInclude?: string[],
+  ): this {
+    if (this.selectFields) {
+      return this;
+    }
+    const result: Record<string, unknown> = {};
+    defaultInclude?.forEach((field) => {
+      if (includeConfig[field]) {
+        result[field] = includeConfig[field];
+      }
+    });
+    const includeParam = this.queryParams.includes as string | undefined;
+
+    if (includeParam && typeof includeParam === "string") {
+      const requestedRelations = includeParam
+        .split(",")
+        .map((relation) => relation.trim());
+
+      requestedRelations.forEach((relation) => {
+        if (includeConfig[relation]) {
+          result[relation] = includeConfig[relation];
+        }
+      });
+    }
+
+    this.query.include = { ...this.query.include, ...result };
+    return this;
+  }
+
   private parseFilterValue(value: unknown): unknown {
     if (value === "true") {
       return true;
