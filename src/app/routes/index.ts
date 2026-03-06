936fa33add06a6f18c059d56aaadac049f7e7d47
rakibul-islam2004 <mdrakibulislamandrokib@gmail.com>
2026-03-06 15:52:21 +0600

    feat(doctor-schedule): implement personal schedule management with transaction support

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -4,6 +4,8 @@ import { AuthRoutes } from "../module/auth/auth.route";
 import { DoctorRoutes } from "../module/doctor/doctor.route";
 import { SpecialtyRoutes } from "../module/specialty/specialty.route";
 import { UserRoutes } from "../module/user/user.route";
+import { scheduleRoutes } from "../module/schedule/schedule.route";
+import { DoctorScheduleRoutes } from "../module/doctorSchedule/doctorSchedule.route";
 
 const router = Router();
 
@@ -12,5 +14,7 @@ router.use("/specialties", SpecialtyRoutes);
 router.use("/users", UserRoutes);
 router.use("/doctors", DoctorRoutes);
 router.use("/admins", AdminRoutes);
+router.use("/schedules", scheduleRoutes);
+router.use("/doctor-schedules", DoctorScheduleRoutes);
 
 export const IndexRoutes = router;
