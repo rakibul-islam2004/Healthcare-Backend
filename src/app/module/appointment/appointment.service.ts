d4b82dca6aa98682282ea5f1d4286e70a9ea4d5c
rakibul-islam2004 <mdrakibulislamandrokib@gmail.com>
2026-03-09 01:00:30 +0600

    feat(appointment): implement book appointment service with transaction

---
+++ src/app/module/appointment/appointment.service.ts
@@ -0,0 +1,72 @@
+import { v7 as uuidv7 } from "uuid";
+import { IRequestUser } from "../../interfaces/requestUser.interface";
+import { prisma } from "../../lib/prisma";
+import { IBookAppointmentPayload } from "./appointment.interface";
+
+const bookAppointment = async (
+  payload: IBookAppointmentPayload,
+  user: IRequestUser,
+) => {
+  const patientData = await prisma.patient.findUniqueOrThrow({
+    where: {
+      email: user.email,
+    },
+  });
+
+  const doctorData = await prisma.doctor.findUniqueOrThrow({
+    where: {
+      id: payload.doctorId,
+      isDeleted: false,
+    },
+  });
+
+  const scheduleData = await prisma.schedule.findUniqueOrThrow({
+    where: {
+      id: payload.scheduleId,
+    },
+  });
+
+  await prisma.doctorSchedules.findUniqueOrThrow({
+    where: {
+      doctorId_scheduleId: {
+        doctorId: doctorData.id,
+        scheduleId: scheduleData.id,
+      },
+    },
+  });
+
+  const videoCallingId = uuidv7();
+
+  const result = await prisma.$transaction(async (tx) => {
+    const appointmentData = await tx.appointment.create({
+      data: {
+        doctorId: payload.doctorId,
+        patientId: patientData.id,
+        scheduleId: payload.scheduleId,
+        videoCallingId,
+      },
+    });
+
+    await tx.doctorSchedules.update({
+      where: {
+        doctorId_scheduleId: {
+          doctorId: payload.doctorId,
+          scheduleId: payload.scheduleId,
+        },
+      },
+      data: {
+        isBooked: true,
+      },
+    });
+
+    // Todo: Payment integration will be here
+
+    return appointmentData;
+  });
+  
+  return result;
+};
+
+export const AppointmentService = {
+  bookAppointment,
+};
